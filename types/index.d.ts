b2f287851ce77778959f434bed4617f5ccc19929
Leoxyzua <apinky549@gmail.com>
2021-10-21 14:47:39 -0500

    some typings fix

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,8 +6,10 @@ export interface Data {
 
 export function allFumos(): Promise<Data[]>;
 
-export function getFumoByID(id: string, allData: boolean): Promise<Data>;
+export function getFumoByID(id: string, allData: true): Promise<Data>;
+export function getFumoByID(id: string, allData?: boolean): Promise<string>;
 export function getFumoByID(id: string): Promise<string>;
 
-export function randomFumo(allData: boolean): Promise<Data>;
+export function randomFumo(allData: true): Promise<Data>;
+export function randomFumo(allData?: boolean): Promise<string>;
 export function randomFumo(): Promise<string>;
