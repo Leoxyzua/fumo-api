1f569ba66b9a313d3fd61c44036f433db06ef58f
Leoxyzua <apinky549@gmail.com>
2021-11-09 00:00:00 -0500

    IM SO DUMB

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import { FumoClient } from './structures/Client';
-
-const client = new FumoClient(true);
-
-// Wait a bit for the fumos to get cached
-setTimeout(function () {
-    console.log(`Random fumo: ${client.cache.random.URL}`)
-}, 2500)
+export * from './structures/Client';
